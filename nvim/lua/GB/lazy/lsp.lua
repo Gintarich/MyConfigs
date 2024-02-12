677de2f79ade0cd3d2fb38816587018797072a4a
Gintarich <gintars.briedis@gmail.com>
2024-02-13 00:42:44 +0200

    Added clanged to mason lsp ensure isntalled

--- nvim/lua/GB/lazy/lsp.lua
+++ nvim/lua/GB/lazy/lsp.lua
@@ -19,7 +19,7 @@ return {
 
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls","omnisharp",},
+            ensure_installed = {"lua_ls","omnisharp","clangd"},
             handlers = {
                 function(server_name) -- default handler (optional)
                     if server_name == "lua_ls" then
