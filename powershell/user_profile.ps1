197399f0f2d094f7c9fa9f4ae4eb324c1ebe32d0
Gintarich <gintars.briedis@gmail.com>
2024-02-13 23:01:51 +0200

    removed function

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -32,16 +32,18 @@ function whereis($command){
 	Get-Command -Name $command -ErrorAction SilentlyContinue |
 	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 	}
-function prompt {
-  $loc = $executionContext.SessionState.Path.CurrentLocation;
 
-  $out = ""
-  if ($loc.Provider.Name -eq "FileSystem") {
-    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-  }
-  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-  return $out
-}
+#function prompt {
+#  $loc = $executionContext.SessionState.Path.CurrentLocation;
+#
+#  $out = ""
+#  if ($loc.Provider.Name -eq "FileSystem") {
+#    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+#  }
+#  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+#  return $out
+#}
+
 #Terminal Icons
 #Import-Module Terminal-Icons
 
