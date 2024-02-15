fc85f8a15222a29427fbddea4cdb57350044776f
Gintarich <gintars.briedis@gmail.com>
2024-02-15 19:00:44 +0200

    Added some code action functions

--- nvim/lua/GB/lazy_init.lua
+++ nvim/lua/GB/lazy_init.lua
@@ -13,4 +13,52 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("GB.lazy")
 
+local opts = { noremap=true, silent=true }
 
+----------------------------------------------------------
+local function quickfix()
+    vim.lsp.buf.code_action({
+        filter = function(a) return a.isPreferred end,
+        apply = true
+    })
+end
+
+vim.keymap.set('n', '<leader>qf', quickfix, opts)
+
+
+local make_code_action_params = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {
+        diagnostics = vim.lsp.diagnostic.get_line_diagnostics()
+    }
+    return params
+end
+
+execute_code_action = function(kind)
+    if not kind then return end
+    local params = make_code_action_params()
+    params.context.only = { kind }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, 'utf-8')
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
+end
+
+list_code_action_kinds = function()
+    local params = make_code_action_params()
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for _, res in pairs(result or {}) do
+        print('---', 'CODE ACTIONS')
+        for _, r in pairs(res.result or {}) do
+            -- vim.pretty_print(r) 
+            print(r.kind)
+        end
+        print('---')
+    end
+end
