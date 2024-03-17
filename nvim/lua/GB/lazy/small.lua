d2f5375ccf94b467f90896d13dd2c9f13c2ed4cf
Gintarich <gintars.briedis@gmail.com>
2024-03-17 17:19:10 +0200

    fixed gitsigns

--- nvim/lua/GB/lazy/small.lua
+++ nvim/lua/GB/lazy/small.lua
@@ -2,17 +2,17 @@ return {
     { 'numToStr/Comment.nvim', opts = {} },
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-    -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    -- 'lewis6991/gitsigns.nvim',
-    -- opts = {
-    --     signs = {
-    --         add = { text = '+' },
-    --         change = { text = '~' },
-    --         delete = { text = '_' },
-    --         topdelete = { text = '‾' },
-    --         changedelete = { text = '~' },
-    --     },
-    -- },},
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        signs = {
+            add = { text = '+' },
+            change = { text = '~' },
+            delete = { text = '_' },
+            topdelete = { text = '‾' },
+            changedelete = { text = '~' },
+        },
+    },},
 
     { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
