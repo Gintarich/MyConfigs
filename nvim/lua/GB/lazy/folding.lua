d546eb674e86bd0bc776c76eb98b9064a2ff6605
Gintarich <gintars.briedis@gmail.com>
2024-03-17 18:47:38 +0200

    Updated harpoon mappings

--- nvim/lua/GB/lazy/folding.lua
+++ nvim/lua/GB/lazy/folding.lua
@@ -8,11 +8,23 @@ return {
             -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
             vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
             vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-            require('ufo').setup({
-                provider_selector = function(bufnr, filetype, buftype)
-                    return { 'treesitter', 'indent' }
-                end
-            })
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+            }
+            local language_servers = require("lspconfig").util.available_servers()
+            for _, ls in ipairs(language_servers) do
+                require("lspconfig")[ls].setup({
+                    capabilities = capabilities,
+                })
+            end
+            require("ufo").setup()
+            -- require('ufo').setup({
+            --     provider_selector = function(bufnr, filetype, buftype)
+            --         return { 'treesitter', 'indent' }
+            --     end
+            -- })
         end
     },
     {
--- nvim/lua/GB/lazy/harpoon.lua
+++ nvim/lua/GB/lazy/harpoon.lua
@@ -7,9 +7,9 @@ return{
         vim.keymap.set("n","<leader>a",mark.add_file,{desc = "[A]dd file to harpoon list"})
         vim.keymap.set("n","<C-e>",ui.toggle_quick_menu)
 
-        vim.keymap.set("n","<C-a>", function() ui.nav_file(1) end)
-        vim.keymap.set("n","<C-s>", function() ui.nav_file(2) end)
-        vim.keymap.set("n","<C-d>", function() ui.nav_file(3) end)
-        vim.keymap.set("n","<C-f>", function() ui.nav_file(4) end)
+        vim.keymap.set("n",'ā', function() ui.nav_file(1) end)
+        vim.keymap.set("n","š", function() ui.nav_file(2) end)
+        vim.keymap.set("n","ž", function() ui.nav_file(3) end)
+        vim.keymap.set("n","č", function() ui.nav_file(4) end)
     end,
 }
