97b8f9b62976aaf059abf1eb4633f165a44ad6f6
Gintarich <gintars.briedis@gmail.com>
2024-03-20 22:34:35 +0200

    Fixed minor bug

--- nvim/lua/GB/init.lua
+++ nvim/lua/GB/init.lua
@@ -20,7 +20,6 @@ local hoverLogic = function()
     local winid = require('ufo').peekFoldedLinesUnderCursor()
     if not winid then
         -- choose one of coc.nvim and nvim lsp
-        vim.fn.CocActionAsync('definitionHover') -- coc.nvim
         vim.lsp.buf.hover()
     end
 end
